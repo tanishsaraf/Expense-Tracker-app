62704ee37d5da1e632407767101010ba1d6c3eff
tanishsaraf <tanishsaraf0709@gmail.com>
2025-01-20 19:35:52 +0530

    first Commit

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,7 +1,8 @@
-export const setAvatarAPI = `${process.env.host}/api/auth/setAvatar`;
-export const registerAPI = `${process.env.host}/api/auth/register`;
-export const loginAPI = `${process.env.host}/api/auth/login`;
-export const addTransaction = `${process.env.host}/api/v1/addTransaction`;
-export const getTransactions = `${process.env.host}/api/v1/getTransaction`;
-export const editTransactions = `${process.env.host}/api/v1/updateTransaction`;
-export const deleteTransactions = `${process.env.host}/api/v1/deleteTransaction`;
+const host="http://localhost:5000"
+export const setAvatarAPI = `${host}/api/auth/setAvatar`;
+export const registerAPI = `${host}/api/auth/register`;
+export const loginAPI = `${host}/api/auth/login`;
+export const addTransaction = `${host}/api/v1/addTransaction`;
+export const getTransactions = `${host}/api/v1/getTransaction`;
+export const editTransactions = `${host}/api/v1/updateTransaction`;
+export const deleteTransactions = `${host}/api/v1/deleteTransaction`;
